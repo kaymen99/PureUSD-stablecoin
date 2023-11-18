5e0753628c80e44f39cf60279cb8fbefbcd2d49f
Aymen <AymenMir1001@gmail.com>
2023-11-18 18:05:27 +0100

    Update IDSCToken.sol

--- src/interfaces/IDSCToken.sol
+++ src/interfaces/IDSCToken.sol
@@ -2,8 +2,12 @@
 
 pragma solidity ^0.8.18;
 
-interface IDSCToken {
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IDSCToken is IERC20 {
     function mint(address to, uint256 amount) external returns (bool);
 
     function burn(uint256 amount) external;
+
+    function burnFrom(address account, uint256 amount) external;
 }
